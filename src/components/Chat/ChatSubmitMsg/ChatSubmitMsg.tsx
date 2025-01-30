2f8f5fbd6af844e741d930fa8bebfa6c1c30f448
iZeevens <izeevens@gmail.com>
2025-01-30 17:28:05 +0500

    feat: add msg state

--- src/components/Chat/ChatSubmitMsg/ChatSubmitMsg.tsx
+++ src/components/Chat/ChatSubmitMsg/ChatSubmitMsg.tsx
@@ -1,10 +1,16 @@
 import styles from "./ChatSubmitMsg.module.css"
+import { useState } from "react"
 
 function ChatSubmitMsg() {
+  const [message, setMessage] = useState<string>('');
+
+  const setNewMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value)
+  }
 
   return (
     <div className={styles.conatiner}>
-      <input className={styles.input} type="text" />
+      <input className={styles.input} type="text" value={message} onChange={(e) => setNewMessage(e)} />
       <button className={styles.submitBtn}>Send</button>
     </div>
   )
