cbb7248922414bd4d1148d04c59e9860d195244e
iZeevens <izeevens@gmail.com>
2025-02-05 15:33:10 +0500

    feat: add scroll for msgs

--- src/components/Chat/ChatMsgs/ChatMsgs.tsx
+++ src/components/Chat/ChatMsgs/ChatMsgs.tsx
@@ -1,4 +1,5 @@
 import styles from "./ChatMsgs.module.css";
+import { useEffect, useRef } from "react";
 import { IMessage } from "../../../types/types";
 
 interface IChatMsgsProps {
@@ -7,8 +8,16 @@ interface IChatMsgsProps {
 }
 
 function ChatMsgs({ messages, error }: IChatMsgsProps) {
+  const contianerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!contianerRef.current) return
+
+    contianerRef.current.scrollTop = contianerRef.current.scrollHeight
+  }, [messages])
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={contianerRef}>
       {error ? <span className={styles.error}>{error}</span> : ''}
       {messages ? messages.map((item) =>
         item.textMessage || item.extendedTextMessage?.text ? (
